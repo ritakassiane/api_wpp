bb0dc8d42612381745270921cb474ec11a1ab349
Rita Kassiane <helenawestphal@Mac-mini-de-Rita.local>
2024-11-12 22:31:07 -0300

    :sparkles: Add: Cria usuario + processa compra/pedido

--- api_wpp/urls.py
+++ api_wpp/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('products/', views.create_product, name='create_product'),
     path('products/<int:pk>/', views.update_product, name='update_product'),
     path('products/<int:pk>/', views.delete_product, name='delete_product'),
+    path('process_sale/', views.process_sale, name='process_sale'),
 ]
--- api_wpp/views.py
+++ api_wpp/views.py
@@ -1,8 +1,54 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
-from .models import Product
+from .models import Customer, Product, Order, OrderItem
 from .serializers import ProductSerializer
+from django.db import transaction
+import decimal
+
+@api_view(['POST'])
+@transaction.atomic
+def process_sale(request):
+    data = request.data
+
+    # Verifique e crie o cliente, se necessário
+    customer_data = data.get("customer")
+    customer, created = Customer.objects.get_or_create(
+        email=customer_data["email"],
+        defaults={
+            "name": customer_data["name"],
+            "phone": customer_data["phone_number"],
+        }
+    )
+
+    # Processa cada produto no pedido e cria, se não existir
+    products_data = data.get("products", [])
+    product_instances = []
+    for product_data in products_data:
+        product, created = Product.objects.get_or_create(
+            name=product_data["name"],
+            defaults={
+                "price": decimal.Decimal(product_data["price"].replace("R$", "").replace(",", ".").strip()),
+            }
+        )
+        product_instances.append(product)
+
+    # Cria o pedido associado ao cliente
+    order = Order.objects.create(
+        customer=customer,
+        price=decimal.Decimal(data["total_price"].replace("R$", "").replace(",", ".").strip()),
+    )
+
+    # Cria os itens do pedido
+    for product_data, product in zip(products_data, product_instances):
+        OrderItem.objects.create(
+            order=order,
+            product=product,
+            quantity=1,  # Defina a quantidade conforme necessário
+            item_price=decimal.Decimal(product_data["price"].replace("R$", "").replace(",", ".").strip())
+        )
+
+    return Response({"message": "Sale processed successfully"}, status=status.HTTP_201_CREATED)
 
 @api_view(['POST'])
 def create_product(request):
